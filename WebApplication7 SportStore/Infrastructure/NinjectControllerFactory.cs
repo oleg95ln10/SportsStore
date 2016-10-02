8350376798e9a8514c5bb4085e014f97d8e8ffe0
Oleg <Oleg@OLEG-PC>
2016-10-02 21:41:06 +0300

    Config DI container

---
+++ WebApplication7 SportStore/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,34 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication7_SportStore.Infrastructure
+{
+    // реализация пользовательской фабрики контроллеров,
+    // наследуясь от фабрики используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            // создание контейнера
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,
+            Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            // конфигурирование контейнера
+        }
+    }
+}
